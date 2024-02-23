2a7539a338e06a13cf6e2117332f179cbd05fc79
Kevin O' Halloran <kevinoh45@gmail.com>
2024-02-23 16:28:23 +0000

    Add: initial test function to ask user their name

--- run.py
+++ run.py
@@ -1,3 +1,9 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+def get_user_name():
+    """Ask user's name to begin"""
+    user_name = input("What is your name? \n")
+    print(f"Welcome {user_name}!")
+
+get_user_name()
