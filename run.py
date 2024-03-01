31d25f9413ffc4334cc87fc06c80f435f7819b1d
Kevin O' Halloran <kevinoh45@gmail.com>
2024-03-01 21:43:44 +0000

    Edit: import title function to run

--- run.py
+++ run.py
@@ -1,6 +1,5 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+from start import print_title
+
 def get_user_name():
     """Ask user's name to begin"""
     user_name = input("What is your name? \n")
@@ -10,4 +9,5 @@ def get_user_name():
         print("You must only enter letters please")
         get_user_name()
 
+print_title()
 get_user_name()
