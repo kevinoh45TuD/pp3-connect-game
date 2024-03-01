003e971267db09307075fa3892529acf56610936
Kevin O' Halloran <kevinoh45@gmail.com>
2024-03-01 22:49:34 +0000

    Edit: added extra rows/columns to match connect 4 board

--- board.py
+++ board.py
@@ -8,10 +8,19 @@ def print_board():
     tile = """###\n###\n###"""
 
     table = Table(title="Connect 4", box=box.DOUBLE, style="blue3", show_lines=True)
+
     table.add_column(tile, style="bright_black", no_wrap=True, justify="center", width=7)
     table.add_column(tile, style="bright_black", justify="center", width=7)
     table.add_column(tile, style="bright_black", justify="center", width=7)
-    table.add_row(tile, tile, tile)
-    table.add_row(tile, tile, tile)
+    table.add_column(tile, style="bright_black", justify="center", width=7)
+    table.add_column(tile, style="bright_black", justify="center", width=7)
+    table.add_column(tile, style="bright_black", justify="center", width=7)
+    table.add_column(tile, style="bright_black", justify="center", width=7)
+
+    table.add_row(tile, tile, tile, tile, tile, tile, tile)
+    table.add_row(tile, tile, tile, tile, tile, tile, tile)
+    table.add_row(tile, tile, tile, tile, tile, tile, tile)
+    table.add_row(tile, tile, tile, tile, tile, tile, tile)
+    table.add_row(tile, tile, tile, tile, tile, tile, tile)
 
     console.print(table)
