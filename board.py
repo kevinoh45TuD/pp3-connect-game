44809f8ef986a926c9b5ef6f208b52b49a6a23da
Kevin O' Halloran <kevinoh45@gmail.com>
2024-03-01 23:07:15 +0000

    Edit: added align to center entire table, reduced cell width

--- board.py
+++ board.py
@@ -1,6 +1,7 @@
 from rich.console import Console
 from rich.table import Table
 from rich import box
+from rich.align import Align
 
 def print_board():
     console = Console()
@@ -9,18 +10,20 @@ def print_board():
 
     table = Table(title="Connect 4", box=box.DOUBLE, style="blue3", show_lines=True)
 
-    table.add_column(tile, style="bright_black", no_wrap=True, justify="center", width=7)
-    table.add_column(tile, style="bright_black", justify="center", width=7)
-    table.add_column(tile, style="bright_black", justify="center", width=7)
-    table.add_column(tile, style="bright_black", justify="center", width=7)
-    table.add_column(tile, style="bright_black", justify="center", width=7)
-    table.add_column(tile, style="bright_black", justify="center", width=7)
-    table.add_column(tile, style="bright_black", justify="center", width=7)
+    table.add_column(tile, style="bright_black", no_wrap=True, justify="center", width=5)
+    table.add_column(tile, style="bright_black", justify="center", width=5)
+    table.add_column(tile, style="bright_black", justify="center", width=5)
+    table.add_column(tile, style="bright_black", justify="center", width=5)
+    table.add_column(tile, style="bright_black", justify="center", width=5)
+    table.add_column(tile, style="bright_black", justify="center", width=5)
+    table.add_column(tile, style="bright_black", justify="center", width=5)
 
     table.add_row(tile, tile, tile, tile, tile, tile, tile)
     table.add_row(tile, tile, tile, tile, tile, tile, tile)
     table.add_row(tile, tile, tile, tile, tile, tile, tile)
     table.add_row(tile, tile, tile, tile, tile, tile, tile)
     table.add_row(tile, tile, tile, tile, tile, tile, tile)
+
+    table = Align.center(table, vertical="middle")
 
     console.print(table)
