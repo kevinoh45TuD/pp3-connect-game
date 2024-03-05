ec24a4e27021defddc400ca9a719ed8b2eea4e04
Kevin O' Halloran <kevinoh45@gmail.com>
2024-03-05 15:10:53 +0000

    Edit: moved ask name function into start file

--- connect/start.py
+++ connect/start.py
@@ -5,3 +5,12 @@ def print_title():
         #     #  #  #   #  #   #  ####  #       #
         #     #  #  #   #  #   #  #     #       #
          ###  ####  #   #  #   #  ####   ###    #\n""")
+
+def get_user_name():
+    """Ask user's name to begin"""
+    user_name = input("What is your name? \n")
+    if user_name.isalpha():
+        print(f"Welcome {user_name}!")
+    else:
+        print("You must only enter letters please")
+        get_user_name()
--- run.py
+++ run.py
@@ -1,23 +1,5 @@
-import time
-from start import print_title
-from screen import clear_screen
-from board import print_board
 
-def get_user_name():
-    """Ask user's name to begin"""
-    user_name = input("What is your name? \n")
-    if user_name.isalpha():
-        print(f"Welcome {user_name}!")
-    else:
-        print("You must only enter letters please")
-        get_user_name()
-
-#print_title()
+#import time
 #time.sleep(3)
-clear_screen()
-#get_user_name()
-#time.sleep(3)
-print_board()
-
 #pip3 freeze > requirements.txt
 #rich==13.7.1
