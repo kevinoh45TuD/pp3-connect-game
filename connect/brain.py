3702e83230250642457420b5990f64f261af5181
Kevin O' Halloran <kevinoh45@gmail.com>
2024-03-05 15:49:29 +0000

    Add: created brain file to contain class for storing game info, added description comments

---
+++ connect/brain.py
@@ -0,0 +1,9 @@
+# File to contain 'Brain' class to contain all relevant info for current game
+
+class Brain:
+    """
+    Will store game information, what value for each tile of board.
+    Print board with this information.
+    Calculate if there is a successfull connect!
+    """
+    
